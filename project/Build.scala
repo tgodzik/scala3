24a2d8a1a9c38ea274565dbbea5d70af258c3d3d
Tomasz Godzik <tgodzik@virtuslab.com>
2025-08-28 11:20:06 +0200

    chore: Set new reference and base versions

--- project/Build.scala
+++ project/Build.scala
@@ -87,7 +87,7 @@ object Build {
    *
    *  Warning: Change of this variable needs to be consulted with `expectedTastyVersion`
    */
-  val referenceVersion = "3.3.6"
+  val referenceVersion = "3.3.7-RC1"
 
   /** Version of the Scala compiler targeted in the current release cycle
    *  Contains a version without RC/SNAPSHOT/NIGHTLY specific suffixes
@@ -98,7 +98,7 @@ object Build {
    *
    *  Warning: Change of this variable might require updating `expectedTastyVersion`
    */
-  val developedVersion = "3.3.7"
+  val developedVersion = "3.3.8"
 
   /** The version of the compiler including the RC prefix.
    *  Defined as common base before calculating environment specific suffixes in `dottyVersion`
