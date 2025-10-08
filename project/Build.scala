16a649cc5ecb34073e49382ef8da9d7d38536098
Tomasz Godzik <tgodzik@virtuslab.com>
2025-10-08 19:12:37 +0200

    chore: Update referenceVersion to 3.3.9 amd add release notes

--- project/Build.scala
+++ project/Build.scala
@@ -98,7 +98,7 @@ object Build {
    *
    *  Warning: Change of this variable might require updating `expectedTastyVersion`
    */
-  val developedVersion = "3.3.8"
+  val developedVersion = "3.3.9"
 
   /** The version of the compiler including the RC prefix.
    *  Defined as common base before calculating environment specific suffixes in `dottyVersion`
