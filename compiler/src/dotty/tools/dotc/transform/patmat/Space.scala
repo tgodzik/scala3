3f9878bef1c27adf21929f3ecbde763ffbb22ff3
Jeon Yoonjae <yoonjae.jeon@devsisters.com>
2025-10-27 06:43:04 +0900

    Use upper bound of abstract types in exhaustivity checking (#23909)
    closes https://github.com/scala/scala3/issues/23620,
    https://github.com/scala/scala3/issues/24246
    
    If the upper bound of an abstract type is not `sealed` but is
    effectively sealed, it is not handled correctly, since classSym could
    return a type that is not `sealed` (Object in the issue above).
    If the type were not abstract, it would pass the logic that checks
    whether it is an Or, And, etc., and would be handled properly.
    This PR makes exhaustivity checking use the upper bound of abstract
    types that are effectively sealed.
    
    ---------
    
    Co-authored-by: Zieliński Patryk <75637004+zielinsky@users.noreply.github.com>

--- compiler/src/dotty/tools/dotc/transform/patmat/Space.scala
+++ compiler/src/dotty/tools/dotc/transform/patmat/Space.scala
@@ -687,6 +687,8 @@ object SpaceEngine {
           else NoType
         }.filter(_.exists)
         parts
+      case tref: TypeRef if tref.isUpperBoundedAbstract =>
+        rec(tref.info.hiBound, mixins)
       case _ => ListOfNoType
     end rec
 
@@ -701,6 +703,10 @@ object SpaceEngine {
         && cls.isOneOf(AbstractOrTrait) // ignore sealed non-abstract classes
         && !cls.hasAnonymousChild       // can't name anonymous classes as counter-examples
         && cls.children.nonEmpty        // can't decompose without children
+
+  extension (tref: TypeRef)
+    def isUpperBoundedAbstract(using Context): Boolean =
+      tref.symbol.isAbstractOrAliasType && !tref.info.hiBound.isNothingType
 
   val ListOfNoType    = List(NoType)
   val ListOfTypNoType = ListOfNoType.map(Typ(_, decomposed = true))
@@ -826,7 +832,11 @@ object SpaceEngine {
       classSym.is(Case) && {
         if seen.add(classSym) then productSelectorTypes(tpw, sel.srcPos).exists(isCheckable(_))
         else true // recursive case class: return true and other members can still fail the check
-      }
+      } ||
+      (tpw.isInstanceOf[TypeRef] && {
+        val tref = tpw.asInstanceOf[TypeRef]
+        tref.isUpperBoundedAbstract && isCheckable(tref.info.hiBound)
+      })
 
     !sel.tpe.hasAnnotation(defn.UncheckedAnnot)
     && {
