2745e1122040b3e302a08f0aef2a6318f5170e96
Tomasz Godzik <tgodzik@virtuslab.com>
2025-11-05 13:48:36 +0100

    No mixin forwarder when ancestor is sealed
    [Cherry-picked 545fdd9118d33db6b86a2e6087d8f7eaad25bbfc][modified]

--- compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
+++ compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
@@ -320,7 +320,7 @@ private sealed trait XSettings:
   val XmixinForceForwarders = ChoiceSetting(
     name    = "-Xmixin-force-forwarders",
     helpArg = "mode",
-    descr   = "Generate forwarder methods in classes inhering concrete methods from traits.",
+    descr   = "Generate forwarder methods in classes inheriting concrete methods from traits.",
     choices = List("true", "junit", "false"),
     default = "true")
 
