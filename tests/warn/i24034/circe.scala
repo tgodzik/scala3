c79922148e28159b25963a41137847c7bdd2db96
Tomasz Godzik <tgodzik@virtuslab.com>
2025-11-05 14:06:33 +0100

    bugfix: Fix issues with warnings, not related to this PR

--- tests/warn/i24034/circe.scala
+++ tests/warn/i24034/circe.scala
@@ -49,11 +49,11 @@ object ConfiguredCodec:
       q: Quotes
   ): Expr[ConfiguredCodec[A]] = {
     mirror match {
-      case '{
-            ${ _ }: Mirror.ProductOf[A] {
-              type MirroredLabel = l
-              type MirroredElemLabels = el
-              type MirroredElemTypes = et
+      case '{ // LTS specific warnings
+            ${ _ }: Mirror.ProductOf[A] { // warn
+              type MirroredLabel = l // warn
+              type MirroredElemLabels = el // warn
+              type MirroredElemTypes = et // warn
             }
           } =>
         '{
