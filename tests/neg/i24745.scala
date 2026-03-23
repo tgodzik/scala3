a3b09a17e67ef965e18e046b7c1e3bed2b3dc328
Tomasz Godzik <tgodzik@virtuslab.com>
2026-03-23 16:05:09 +0100

    bugfix: Fix tests that include unsupported features

--- tests/neg/i24745.scala
+++ tests/neg/i24745.scala
@@ -1,6 +1,6 @@
 extension (s: String)
   def f_::(using DummyImplicit): String = s.reverse
-  def g_::(x: (suffix: String, n: Int)): String = s"$s${x.suffix * x.n}"
+  def g_::(x: (String, Int)): String = s"$s${x._1 * x._2}"
   def ok_::(using suffix: String, n: Int): String = s"$s${suffix * n}"
   def no_::(suffix: String, n: Int): String = s"$s${suffix * n}" // error
   def huh_::(using DummyImplicit)(suffix: String, n: Int): String = s"$s${suffix * n}"
@@ -10,19 +10,25 @@ def local =
   "hello, world".f_::
 
 @main def Test =
-  println:
+  println(
     "hello, world".f_::
-  println:
+  )
+  println(
     f_::
       ("hello, world")
       (using DummyImplicit.dummyImplicit)
-  println:
-    (suffix = "s", n = 3).g_::("hello, world")
-  println:
+  )
+  println(
+    ("s", 3).g_::("hello, world")
+  )
+  println(
     "hello, world" g_:: ("s", 3)
-  println:
+  )
+  println{
     given String = "s"
     given Int = 3
     "hello, world".ok_::
-  println:
+  }
+  println(
     "hello, world".huh_::("s", 3)
+  )
